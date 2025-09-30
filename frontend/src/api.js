1344f9e22f6a74fc3a34ba3b8627c68523a51392
Sam Nithish <nithishraj8055@gmail.com>
2025-09-30 14:01:59 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://foodbridgebackend-lqo2.onrender.com/api",
+  baseURL: "https://foodbridge-cwfl.onrender.com/api",
 });
 
 // Add token automatically
